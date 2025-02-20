e9f00359eb1c37d5f6ebce3fd916053e5953461b
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2025-02-20 08:20:52 +0100

    Update xlsxWorker.js

--- xlsxWorker.js
+++ xlsxWorker.js
@@ -14,7 +14,6 @@ self.onmessage = async function(event) {
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
         let jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-        jsonData = jsonData.map;
         
         let label;
         if (index >= 0 && index <= 2) label = 'Goalkeeper';
